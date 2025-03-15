26a72afb16047f9c9c811bd02bfa5d5cc46d1ea8
alkaifaftab000 <alkaif.aftab000@outlook.com>
2025-03-16 04:07:15 +0530

    "ready for deployment"

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 from flask import Flask, request, render_template
-from pyngrok import ngrok  # Import pyngrok directly
 import numpy as np
 from tensorflow import keras
 import tensorflow as tf
-
 from PIL import Image
-
 import base64
 from io import BytesIO
 import os
 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-
-
-
-
-ngrok.set_auth_token("2pHSGjH4neJQ7ieRtHuK70iGoEA_5Lw5Ss5mudGxsaFcfcMKc")
 
 app = Flask(__name__)
 
@@ -29,8 +21,6 @@ def pil_to_b64(pil_img):
 
 model = keras.models.load_model('model/imageclassifier.h5')
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-
 
 @app.route('/')
 def home():
@@ -60,9 +50,5 @@ def predict():
             return f"Error processing image: {e}"
 
 if __name__ == '__main__':
-    # Start the ngrok tunnel to the Flask app's port
-    public_url = ngrok.connect(5000)  # bind_tls=True enables HTTPS
-    print("Public URL for the app:", public_url)  # Print the public URL to access the app
-
-    # Start the Flask app
-    app.run()
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
