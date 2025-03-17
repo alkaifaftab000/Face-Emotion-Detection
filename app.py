a8e793245e1c91b1e1325085f263810cf662b3b0
alkaifaftab000 <alkaif.aftab000@outlook.com>
2025-03-17 17:38:50 +0530

    "ready for pythonanywhere"

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, request, render_template
 import numpy as np
-from tensorflow import keras
-import tensorflow as tf
-
+from tflite_runtime.interpreter import Interpreter  # Use tflite-runtime instead of TensorFlow
 from PIL import Image
-
 import base64
 from io import BytesIO
 import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress TensorFlow informational logs
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 app = Flask(__name__)
 
@@ -22,8 +18,13 @@ def pil_to_b64(pil_img):
     base64_str = base64.b64encode(byte_data).decode('utf-8')
     return base64_str
 
-model = keras.models.load_model('model/imageclassifier_keras3.h5')
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# Load the TensorFlow Lite model
+interpreter = Interpreter(model_path='model/imageclassifier_keras3.tflite')
+interpreter.allocate_tensors()
+
+# Get input and output details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 @app.route('/')
 def home():
@@ -40,10 +41,16 @@ def predict():
             # Process image
             img = Image.open(file.stream).convert('RGB')  # Ensure image is in RGB format
             img = img.resize((256, 256))  # Resize using PIL
-            img_array = np.array(img) / 255.0  # Normalize pixel values
+            img_array = np.array(img, dtype=np.float32) / 255.0  # Normalize pixel values
 
-            # Make prediction
-            prediction = model.predict(np.expand_dims(img_array, axis=0))
+            # Prepare input tensor
+            input_data = np.expand_dims(img_array, axis=0)
+            interpreter.set_tensor(input_details[0]['index'], input_data)
+
+            # Run inference
+            interpreter.invoke()
+            prediction = interpreter.get_tensor(output_details[0]['index'])
+
             emotions = 'Happy' if prediction[0] > 0.5 else 'Sad'
             
             return render_template('result.html', prediction=prediction[0], emotions=emotions, imgPath=img)
--- model/update.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-
-model = load_model('imageclassifier.h5')
-model.save('imageclassifier_keras3.h5', save_format='h5')
